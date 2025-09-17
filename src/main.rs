f0240e686a0051a2baf75e590ac5fd1381f8117c
Norman Shipman <norman@nshipman.io>
2025-09-17 07:30:55 -0400

    adding echo builtin

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,25 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-        if input.trim().starts_with("exit") {
-                let exit_code = input.split_whitespace()
-                    .nth(1)
-                    .map(|s| s.parse::<i32>().unwrap_or_else(|_| {
-                       1
-                    }))
-                    .unwrap_or(0);
-                std::process::exit(exit_code);
+        let parts: Vec<&str> = input.trim().split_whitespace().collect();
+        if parts.is_empty() {
+            continue;
         }
 
-        println!("{}: command not found", input.trim().split_whitespace().next().unwrap());
+        let cmd = parts[0];
+        let args = &parts[1..];
+        match cmd {
+            "exit" => {
+                let exit_code = args.get(0)
+                    .and_then(|s| s.parse::<i32>().ok())
+                    .unwrap_or(0);
+                std::process::exit(exit_code);
+            },
+            "echo" => {
+                println!("{}", args.join(" "));
+            },
+            _ => println!("{}: command not found", input.trim().split_whitespace().next().unwrap()),
+
+        }
     }
 }
